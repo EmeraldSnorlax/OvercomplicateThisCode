05edfe38a3ca7b9981a340d26b5fb7bffa31dc5e
X <70487315+XWasHere@users.noreply.github.com>
2021-04-28 12:44:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,29 +9,31 @@ function getFunction(fname, a = globalThis) {
   }
 }
 
-const fs = require("fs");
+const fs = (getFunction("require"))("fs");
 
-let p = fs.readFileSync('./potato.png');
+let p = (getFunction("readFileSync",fs))("./potato.png");
 
 function main() {
   let var1 = "";
   let var2 = "";
   
-  fs.readFile('./hi', 'utf8' , (err, data) => {
+  (getFunction("readFile",fs))('./hi', 'utf8' , (err, data) => {
     if (err) {
-      getConsole().error(err)
+      let c = (getFunction("getConsole"))()
+      (getFunction("error",c))(err)
       return
     }
     var1 = data;
-    fs.readFile('./hi2', 'utf8' , (err, data) => {
+    (getFunction("readFile",fs))('./hi2', 'utf8' , (err, data) => {
       if (err) {
-        getConsole().error(err)
+        let c = (getFunction("getConsole"))()
+        (getFunction("error",c))(err)
         return
       }
       var2 = data.substring(0, data.length-1);
-      getConsole().log((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
+      getFunction("log",(getFunction("getConsole"))())((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
     })
   })
 }
 
-main();
+(getFunction("main"))();
