b370caf4b9aa08b2ca255f0b07addf18f6c3ac3f
X <70487315+XWasHere@users.noreply.github.com>
2021-04-28 12:33:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function getConsole() {
+  return console;
+}
+
 const fs = require("fs");
 
 let p = fs.readFileSync('./potato.png');
@@ -8,18 +12,19 @@ function main() {
   
   fs.readFile('./hi', 'utf8' , (err, data) => {
     if (err) {
-      console.error(err)
+      getConsole().error(err)
       return
     }
     var1 = data;
     fs.readFile('./hi2', 'utf8' , (err, data) => {
       if (err) {
-        console.error(err)
+        getConsole().error(err)
         return
       }
       var2 = data.substring(0, data.length-1);
-      console.log((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
+      getConsole().log((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
     })
   })
 }
+
 main();
