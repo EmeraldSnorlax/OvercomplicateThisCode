338b4a66113bbee8eb5781d380aa84871c3479c3
X <70487315+XWasHere@users.noreply.github.com>
2021-04-28 12:04:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const { fs } = require("fs");
 
-let var1 = "";
-let var2 = "";
+function main() {
+  let var1 = "";
+  let var2 = "";
 
-fs.readFile('./hi', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  var1 = data;
-})
+  fs.readFile('./hi', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    var1 = data;
+  })
 
-fs.readFile('./hi2', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  var2 = data;
-})
+  fs.readFile('./hi2', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    var2 = data;
+  })
 
-console.log((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!");
+  console.log((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!");
+}
+main();
