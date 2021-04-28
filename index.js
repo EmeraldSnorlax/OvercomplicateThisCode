9e9c90d8e20d27505463782dca0baaca0ffb4eaf
X <70487315+XWasHere@users.noreply.github.com>
2021-04-28 15:18:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-let a = {}
-a.getConsole = () => {
+let strings = {
+  "a":"fs"
+}
+
+global.getConsole = () => {
   return console;
 }
 
-a.isTrue = (value) => {
+global.isTrue = (value) => {
   if (value) {
     return true
   }
   else return false
 }
 
-a.isFalse = (value) => {
-  if (getFunction("isTrue",a)(value)) {
+global.isFalse = (value) => {
+  if (getFunction("isTrue",global)(value)) {
     return false
   }
   else return true
+}
+
+global.getString = (s) => {
+  
 }
 
 global.getFunction = (fname,a) => {
@@ -30,20 +37,20 @@ function main() {
   let var2 = "";
   
   (getFunction("readFile",fs))('./hi', 'utf8' , (err, data) => {
-    if (getFunction("isTrue",a)(err)) {
-      let c = (getFunction("getConsole",a))()
+    if (getFunction("isTrue",global)(err)) {
+      let c = (getFunction("getConsole",global))()
       (getFunction("error",c))(err)
       return
     }
     var1 = data;
     (getFunction("readFile",fs))('./hi2', 'utf8' , (err, data) => {
-      if (getFunction('isTrue',a)(err)) {
-        let c = (getFunction("getConsole",a))()
+      if (getFunction('isTrue',global)(err)) {
+        let c = (getFunction("getConsole",global))()
         (getFunction("error",c))(err)
         return
       }
       var2 = data.substring(0, data.length-1);
-      getFunction("log",(getFunction("getConsole",a))())((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
+      getFunction("log",(getFunction("getConsole",global))())((var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
     })
   })
 }
