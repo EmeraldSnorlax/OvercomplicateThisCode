e59668b86936d735c5107005e5a088f23b9d15a1
X <70487315+XWasHere@users.noreply.github.com>
2021-04-28 11:07:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 const { execFile } = require("child_process");
 
-let thing = execFile("node", ["./index.js"]);
-thing.stdout.on("data", (d) => {console.log(d.toString()); process.exit()})
+let thing = execFile("node", ["./index.js"])
+
+thing.stdout.on("data", (d) => {
+  console.log(d.toString());
+})
+
+thing.stderr.on("data", (d) => {
+  console.error(d.toString());
+})
