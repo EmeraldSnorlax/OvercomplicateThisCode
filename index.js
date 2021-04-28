3af46656cdcc1b1bfaeb9c8bcdbd58b0b65a395c
X <70487315+XWasHere@users.noreply.github.com>
2021-04-28 15:22:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function main() {
         return
       }
       var2 = data.substring(0, data.length-1);
-      console.log(var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!"));
+      console.log(var2.replace(var2, var1))+" "+(var1.replace(var1, var2)+"!");
     })
   })
 }
